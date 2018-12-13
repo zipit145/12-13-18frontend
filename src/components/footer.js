7ead641952f349f02562a35835dfc3b0cdd86cd0
xwrexx <wrex1285@gmail.com>
2018-12-13 14:12:47 -0700

    styled footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,20 +2,10 @@ import React from 'react'
 
 const Footer = () => {
     return(
-        <footer className="page-footer font-small unique-color-dark pt-4">
-             <div className="container">
-                <ul className="list-unstyled list-inline text-center py-2">
-                    <li className="list-inline-item">
-                    <h5 className="mb-1">Register for free</h5>
-                    </li>
-                    <li className="list-inline-item">
-                    <a href="#!" className="btn btn-outline-white btn-rounded">Sign up!</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="footer-copyright text-center py-3">© 2018 Copyright:
-                <span> Best Cosers Alive</span> 
-            </div>
+        <footer>
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">Copyright 2018: Best coders ever</a>
+            </nav>
         </footer>
     )
 }
